0fd7e43c750dfc3f640d47620725c6d2de755db1
Arthur Duarte <arthurd0605@gmail.com>
2023-10-03 11:27:02 -0300

    upgrade

--- src/aula02/ExercicioO_N/Teste2.java
+++ src/aula02/ExercicioO_N/Teste2.java
@@ -1,9 +1,12 @@
 package aula02.ExercicioO_N;
 
-public class MainExemploOn {
+public class Teste2 {
     public static void main(String[] args) {
         VetorArray vetor = new VetorArray();
         double[] array = {1, 2, 3, 4, 5};
+        // METODO MEDIA
+        System.out.println(vetor.media(array));
+        // METODO EXISTE
         System.out.println(vetor.existe(array, 3));
     }
 }
