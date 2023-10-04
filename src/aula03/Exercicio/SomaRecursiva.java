c6990553b7809aae93f7cf1561b112e007e994f2
Arthur Duarte <arthurd0605@gmail.com>
2023-10-04 12:08:41 -0300

    Exercicio aula 03 adicionado

---
+++ src/aula03/Exercicio/SomaRecursiva.java
@@ -0,0 +1,20 @@
+package aula03.Exercicio;
+
+public class SomaRecursiva {
+    public static int somaArrayRecursivamente(int[] array, int tamanho) {
+        if (tamanho == 0) {
+            return 0;
+        } else {
+            return array[tamanho - 1] + somaArrayRecursivamente(array, tamanho - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {20, 30, 40};
+        int tamanho = array.length;
+        int resultado = somaArrayRecursivamente(array, tamanho);
+        System.out.println("A soma recursiva é: " + resultado);
+
+    }
+
+}
